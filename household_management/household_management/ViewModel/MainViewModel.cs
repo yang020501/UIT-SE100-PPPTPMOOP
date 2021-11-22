ce79bc006e5e988d6d6bfee7c06524b685c34af8
Duy <dragonslayer110464@gmail.com>
2021-11-22 13:07:42 +0700

    Tạo model và base viewmodel

---
+++ household_management/household_management/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace household_management.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
