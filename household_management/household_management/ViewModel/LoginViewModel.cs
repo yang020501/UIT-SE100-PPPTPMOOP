a9775870aff98c53a41586fe6f61445d56481bd9
yang020501 <thaiduong020501@gmail.com>
2021-11-26 13:45:54 +0700

    2

---
+++ household_management/household_management/ViewModel/LoginViewModel.cs
@@ -0,0 +1,59 @@
+﻿using household_management.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace household_management.ViewModel
+{
+    class LoginViewModel : BaseViewModel
+    {
+        public bool isLogin = false;
+        private string _UserName;
+        public string UserName { get => _UserName; set { _UserName = value; OnPropertyChanged(); } }
+        private string _Password;
+        public string Password { get => _Password; set { _Password = value; OnPropertyChanged(); } }
+        public ICommand LoginCommand { get; set; }
+        public ICommand ExistCommand { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
+        public LoginViewModel()
+        {
+            isLogin = false;
+            Password = "";
+            UserName = "";
+            ExistCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { p.Close(); });
+            LoginCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { Login(p); });
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => { Password = p.Password; });
+        }
+
+        void Login(Window p)
+        {
+            if (p == null)
+            {
+                return;
+            }
+            string passEncode = Base64Encode(Password);
+            var accCount = DataProvider.Ins.DB.Users.Where(x => x.Username == UserName && x.Password == passEncode).Count();
+
+            if (accCount > 0)
+            {
+                isLogin = true;
+                p.Close();
+            }
+            else
+            {
+                isLogin = false;
+                MessageBox.Show("Wrong password or user name","Warning!",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }        
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
