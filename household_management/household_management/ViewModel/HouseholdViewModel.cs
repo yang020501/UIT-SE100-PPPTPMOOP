cc7033fe017d5a8b0e23415922a777c65f9d2a63
yang020501 <thaiduong020501@gmail.com>
2021-11-27 15:26:46 +0700

    yangya

--- household_management/household_management/ViewModel/HouseholdViewModel.cs
+++ household_management/household_management/ViewModel/HouseholdViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using household_management.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +12,24 @@ namespace household_management.ViewModel
 {
     class HouseholdViewModel : BaseViewModel
     {
-      
-
+       
+        public ICommand btnClear { get; set; }
         public HouseholdViewModel()
         {
-            
+            btnClear = new RelayCommand<Window>((p) => { return true; }, (p) => { Clear(p); });
+           
+        }
+        private void Clear(Window p)
+        {
+            List<Control> list = new List<Control>();
+          
+
+        }
+        private void GetAllControls(Control container)
+        {
+           
         }
 
-    
     }
+  
 }
