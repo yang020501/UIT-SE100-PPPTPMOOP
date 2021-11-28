62f766d4988ba8be00ca8d4d7b69188d5be6ed13
Duy <dragonslayer110464@gmail.com>
2021-11-28 16:39:27 +0700

    push để lát cúp điện

--- household_management/household_management/ViewModel/HouseholdViewModel.cs
+++ household_management/household_management/ViewModel/HouseholdViewModel.cs
@@ -12,12 +12,40 @@ namespace household_management.ViewModel
 {
     class HouseholdViewModel : BaseViewModel
     {
-       
+        private string _Name;
+        public string Name { get => _Name; set { _Name = value; OnPropertyChanged(); } }
+        private string _Id;
+        public string Id { get => _Id; set { _Id = value; OnPropertyChanged(); } }
+        private string _Address;
+        public string Address { get => _Address; set { _Address = value; OnPropertyChanged(); } }
+        private string IdHousehold;
         public ICommand btnClear { get; set; }
+        public ICommand btnAdd { get; set; }
         public HouseholdViewModel()
         {
             btnClear = new RelayCommand<Window>((p) => { return true; }, (p) => { Clear(p); });
-           
+
+            btnAdd = new RelayCommand<object>((p) =>
+            {
+                if(Id == null || Name == null || Address == null)
+                {
+                    return false;
+                }
+
+                return true;
+            },
+            (p) =>
+            {
+                IdHousehold = GenarateId();
+                Model.Household_Registration x = new Model.Household_Registration();
+                x.Id = IdHousehold;
+                x.IdOfOwner = Id;
+                x.Address = Address;
+                x.NameOfOwner = Name;
+
+                Model.DataProvider.Ins.DB.Household_Registration.Add(x);
+                Model.DataProvider.Ins.DB.SaveChanges();
+            });
         }
         private void Clear(Window p)
         {
@@ -28,6 +56,25 @@ namespace household_management.ViewModel
         private void GetAllControls(Control container)
         {
            
+        }
+
+        private string GenarateId()
+        {           
+            string keychar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string keynum = "0123456789";
+            Random random = new Random();
+            string code = "";
+
+            int num = random.Next(0, 26);
+            code += keychar[num];
+
+            for(int i=0; i<4; i++)
+            {
+                num = random.Next(0, 10);
+                code += keynum[num];
+            }
+
+            return code;
         }
 
     }
