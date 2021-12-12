81ecce500be686dfe8f6101cb8421320d7c4cd2b
yang020501 <thaiduong020501@gmail.com>
2021-12-12 20:57:21 +0700

    Update

--- household_management/household_management/ViewModel/RPVViewModel.cs
+++ household_management/household_management/ViewModel/RPVViewModel.cs
@@ -10,18 +10,14 @@ using System.Windows.Controls;
 
 namespace household_management.ViewModel
 {
-    class APVViewModel : BaseViewModel
+    class RPVViewModel : BaseViewModel
     {
-        
+
         DataTable dt;
 
-        private DataView dvAbsence;
-        public DataView DvAbsence { get => dvAbsence; set { dvAbsence = value; OnPropertyChanged(); } }
+        private DataView dvResidence;
+        public DataView DvResidence { get => dvResidence; set { dvResidence = value; OnPropertyChanged(); } }
 
-<<<<<<< Updated upstream
-        private ObservableCollection<Temporary_Absence> AbsenceList;
-
-=======
         private string _Name;
         public string Name { get => _Name; set { _Name = value; OnPropertyChanged(); } }
 
@@ -34,8 +30,11 @@ namespace household_management.ViewModel
         private string _DateOfBirth;
         public string DateOfBirth { get => _DateOfBirth; set { _DateOfBirth = value; OnPropertyChanged(); } }
 
-        private string _AbAddress;
-        public string AbAddress { get => _AbAddress; set { _AbAddress = value; OnPropertyChanged(); } }
+        private string _PAddress;
+        public string PAddress { get => _PAddress; set { _PAddress = value; OnPropertyChanged(); } }
+
+        private string _TAddress;
+        public string TAddress { get => _TAddress; set { _TAddress = value; OnPropertyChanged(); } }
 
         private string _HName;
         public string HName { get => _HName; set { _HName = value; OnPropertyChanged(); } }
@@ -44,7 +43,7 @@ namespace household_management.ViewModel
         public string Id_Household { get => _Id_Household; set { _Id_Household = value; OnPropertyChanged(); } }
 
         private string _CreateDate;
-        public string CreateDate { get => _CreateDate;set { _CreateDate = value; OnPropertyChanged(); } }
+        public string CreateDate { get => _CreateDate; set { _CreateDate = value; OnPropertyChanged(); } }
 
         private string _ExpireDate;
         public string ExpireDate { get => _ExpireDate; set { _ExpireDate = value; OnPropertyChanged(); } }
@@ -54,7 +53,7 @@ namespace household_management.ViewModel
         private bool _FemaleChoice;
         public bool FemaleChoice { get => _FemaleChoice; set { _FemaleChoice = value; OnPropertyChanged(); } }
 
-        private ObservableCollection<Temporary_Absence> AbsenceList;
+        private ObservableCollection<Temporary_Residence> ResidenceList;
 
         private DataRowView _Selected;
         public DataRowView Selected
@@ -64,14 +63,15 @@ namespace household_management.ViewModel
             {
                 _Selected = value;
                 OnPropertyChanged();
-                if(_Selected != null)
+                if (_Selected != null)
                 {
                     Name = (string)Selected.Row["Name"];
                     IdOwner = (string)Selected.Row["Id_Owner"];
                     Id = (string)Selected.Row["Id"];
-                    AbAddress = (string)Selected.Row["AbsenceAddress"];
+                    PAddress = (string)Selected.Row["PAddress"];
+                    TAddress = (string)Selected.Row["TAddress"];
                     CreateDate = (string)Selected.Row["CreateDate"];
-                    ExpireDate = (string)Selected.Row["ExpireDate"];
+                    _ExpireDate = (string)Selected.Row["ExpireDate"];
                     HName = (string)Selected.Row["Name_HouseholdOwner"];
                     Id_Household = (string)Selected.Row["Id_Household"];
                     if ((string)Selected.Row["Gender"] == "Male")
@@ -83,15 +83,14 @@ namespace household_management.ViewModel
             }
         }
 
->>>>>>> Stashed changes
-        public APVViewModel()
+        public RPVViewModel()
         {
-            NewTableAbsence();
+            NewTableResidence();
         }
 
-        private void NewTableAbsence()
+        private void NewTableResidence()
         {
-            AbsenceList = new ObservableCollection<Temporary_Absence>(DataProvider.Ins.DB.Temporary_Absence);
+            ResidenceList = new ObservableCollection<Temporary_Residence>(DataProvider.Ins.DB.Temporary_Residence);
             dt = new DataTable();
 
             dt.Columns.Add("OrdinalNumber");
@@ -100,50 +99,52 @@ namespace household_management.ViewModel
             dt.Columns.Add("Name");
             dt.Columns.Add("Id_Household");
             dt.Columns.Add("Name_HouseholdOwner");
-            dt.Columns.Add("AbsenceAddress");
+            dt.Columns.Add("PAddress");           
             dt.Columns.Add("CreateDate");
             dt.Columns.Add("ExpireDate");
             dt.Columns.Add("Photo");
             dt.Columns.Add("Gender");
             dt.Columns.Add("DateOfBirth");
+            dt.Columns.Add("TAddress");
             //fill datatable
-            for (int i = 0; i < AbsenceList.Count; i++)
+            for (int i = 0; i < ResidenceList.Count; i++)
             {
 
                 dt.Rows.Add
                     (
-                       //AbsenceList[i].Stt.ToString(),
-                       //AbsenceList[i].Id.ToString(),
-                       //AbsenceList[i].Id_Owner.ToString(),
-                       //AbsenceList[i].NameOfOwner.ToString(),
-                       //AbsenceList[i].Id_Household.ToString(),
-                       //AbsenceList[i].HouseOwnerName.ToString(),
-                       //AbsenceList[i].Household_Registration.Address.ToString(),
-                       //AbsenceList[i].CreateDate.ToString(),
-                       //AbsenceList[i].ExpireDate.ToString()
-                       
-                       CheckData(AbsenceList[i])
-                    ); 
+                       //ResidenceList[i].Stt.ToString(),
+                       //ResidenceList[i].Id.ToString(),
+                       //ResidenceList[i].Id_Owner.ToString(),
+                       //ResidenceList[i].NameOfOwner.ToString(),
+                       //ResidenceList[i].Id_Household.ToString(),
+                       //ResidenceList[i].HouseOwnerName.ToString(),
+                       //ResidenceList[i].Household_Registration.Address.ToString(),
+                       //ResidenceList[i].CreateDate.ToString(),
+                       //ResidenceList[i].ExpireDate.ToString()
+
+                       CheckData(ResidenceList[i])
+                    );
 
             }
-            DvAbsence = new DataView(dt);
+            DvResidence = new DataView(dt);
         }
         // Check if any fields is null
-        private string[] CheckData(Temporary_Absence item)
+        private string[] CheckData(Temporary_Residence item)
         {
-            string[] list = new string[12];
+            string[] list = new string[13];
             list[0] = check(item.Stt);
             list[1] = check(item.Id);
             list[2] = check(item.Id_Owner);
             list[3] = check(item.NameOfOwner);
             list[4] = check(item.Id_Household);
             list[5] = check(item.HouseOwnerName);
-            list[6] = check(item.Household_Registration.Address);
+            list[6] = check(item.PAddress);
             list[7] = check(item.CreateDate);
             list[8] = check(item.ExpireDate);
             list[9] = check(item.Household_Registration.Population.Photo);
             list[10] = check(item.Population.Sex);
             list[11] = check(item.Population.DateOfBirth);
+            list[12] = check(item.TAddress);
             return list;
         }
         // Convert null, string or any type to Valid view data
@@ -172,13 +173,12 @@ namespace household_management.ViewModel
         public void doSearch(DataGrid dtg, string find, string form)
         {
             form += " Like '%{0}%'";
-            if (DvAbsence.Count < 0) // if nothing return 
+            if (DvResidence.Count < 0) // if nothing return 
                 return;
-            DvAbsence.RowFilter = string.Format(form, find);
-            dtg.ItemsSource = DvAbsence;
-            OnPropertyChanged("DvPopulations");
+            DvResidence.RowFilter = string.Format(form, find);
+            dtg.ItemsSource = DvResidence;
+            OnPropertyChanged();
 
         }
     }
 }
-
